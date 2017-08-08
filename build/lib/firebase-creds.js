ff8490cb79f41835d5b2df9128bde0c9aa780f01
cwahlfeldt <chriswahlfeldt@gmail.com>
2017-08-08 14:05:34 -0500

    experiments on heroku

--- build/lib/firebase-creds.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-module.exports = {
-  apiKey: "AIzaSyBe5oWZPngkkFxdc29VPwSqxLcWZSuUyyk",
-  authDomain: "queue-813f1.firebaseapp.com",
-  databaseURL: "https://queue-813f1.firebaseio.com"
-};
